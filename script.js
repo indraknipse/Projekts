131f4bf54016ec0c42640164e197767932fe3af3
indraknipse <indraknipse@gmail.com>
2024-06-17 13:01:52 +0000

    1_praktiskais_darbs_nepabeigts

--- script.js
+++ script.js
@@ -1,47 +1,59 @@
-window.onload=function(){
+window.onload = function() {
   zimetuzkanva();
 }
-function zimetuzkanva(){
+function zimetuzkanva() {
   var canva = document.getElementById("zimejums");
-  var konteksts= canva.getContext("2d");
+  var konteksts = canva.getContext("2d");
   konteksts.fillStyle = "red";
-  konteksts.fillRect(20,20,150,100);
+  konteksts.fillRect(20, 20, 150, 100);
 }
-function taisnsturis(){
+function taisnsturis() {
   var canva = document.getElementById("zimejums");
-  var konteksts= canva.getContext("2d");
+  var konteksts = canva.getContext("2d");
   konteksts.strokeStyle = "red";
-  konteksts.strokeRect(60,60,150,100);
+  konteksts.strokeRect(60, 60, 150, 100);
 }
 function aplis() {
   var kanva = document.getElementById("zimejums");
   var konteksts = kanva.getContext("2d");
   konteksts.beginPath();
-  konteksts.arc(200,200,50,0,2*Math.PI);
+  konteksts.arc(200, 200, 50, 0, 2 * Math.PI);
   konteksts.linweWidth = 20;
   konteksts.fillStyle = "pink";
   konteksts.strokeStyle = "red";
   konteksts.fill();
   konteksts.stroke();
 }
-function linija(){
-    var kanva = document.getElementById("zimejums");
-    var konteksts = kanva.getContext("2d");
-    konteksts.beginPath();
-    konteksts.moveTo(50,300);
-    konteksts.lineTo(250,350);
-    konteksts.linweWidth = 5;
-    konteksts.strokeStyle= "red";
-    konteksts.stroke();
+function linija() {
+  var kanva = document.getElementById("zimejums");
+  var konteksts = kanva.getContext("2d");
+  konteksts.beginPath();
+  konteksts.moveTo(50, 300);
+  konteksts.lineTo(250, 350);
+  konteksts.linweWidth = 5;
+  konteksts.strokeStyle = "red";
+  konteksts.stroke();
 }
-  function teksts(){
-      var kanva = document.getElementById("zimejums");
-      var konteksts = kanva.getContext("2d");
-      konteksts.font = "30px Kristen ITC";
-      konteksts.fillStyle = "pink";
-      konteksts.fillText("Sveika, Pasaule!",250,150);
-  }
-  
+function teksts() {
+  var kanva = document.getElementById("zimejums");
+  var konteksts = kanva.getContext("2d");
+  konteksts.font = "30px Kristen ITC";
+  konteksts.fillStyle = "pink";
+  konteksts.fillText("Sveika, Pasaule!", 250, 150);
+}
 
-    
-    
+function aprekins() {
+  let pirmais = 5;
+  let otrais = 10.5;
+  let summa = pirmais + otrais;
+  console.log("Atbilde ir:" + summa);
+}
+function aprekins() {
+  let pirmais = parseFloat(document.getElementById("a").value);
+  let otrais = parseFloat(document.getElementById("b").value);
+  let summa = pirmais + otrais;
+  console.log("Tevi sauc:" + vards + "Atbilde ir" + summa);
+  document.getElementById("rezultats").innerText = "Tevi sauc:" + vards + "Atbilde ir" + summa;
+}
+
+
